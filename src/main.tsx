ca6d0e0246ef917cbaa459b56096ce90e2e08efc
levidulitz <levi@binghamton.edu>
2025-09-25 20:32:24 -0400

    Switch Back to Simple Local Storage Version

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App-Database.tsx';
+import App from './App-Simple.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
